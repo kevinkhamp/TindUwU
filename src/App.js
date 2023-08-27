ffab1186095b514078237de6effb194176b3c093
Kevin <khampkevin@gmail.com>
2023-08-26 22:58:01 -0400

    resourced and added pages

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Home from "./pages/home";
+import Dashboard from "./pages/dashboard";
+import Onboarding from "./pages/onboarding";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
